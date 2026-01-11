99530d4925e897deed91cfb6d9b399bddd15290e
Deus-lovult <neoaska.eva02@gmail.com>
2026-01-11 15:25:56 +0900

    20260111 B-Trifecta解答

---
+++ B_Trifecta.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class B_Trifecta {
+    public static void main(String[] args){
+        List<Integer> tiList = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        if (!sc.hasNext()) return ;
+        int horseNum = sc.nextInt();
+
+        while (sc.hasNext()){
+            tiList.add(sc.nextInt());
+        }
+
+
+    }
+}
